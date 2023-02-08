48394d315f3726abe5374a25f313110d0698dc73
thecomet28 <47154526+thecomet28@users.noreply.github.com>
2023-02-08 11:17:36 -0500

    Replace with new feed-sync package

--- scripts/sync-all.sh
+++ scripts/sync-all.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Updating NVTs..."
-su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /var/lib/openvas/plugins" gvm
-sleep 5
-
-echo "Updating GVMd data..."
-su -c "greenbone-feed-sync --type GVMD_DATA" gvm
-sleep 5
-
-echo "Updating SCAP data..."
-su -c "greenbone-feed-sync --type SCAP" gvm
-sleep 5
-
-echo "Updating CERT data..."
-su -c "greenbone-feed-sync --type CERT" gvm
+echo "Downloading Feed Data..."
+su -c "/usr/local/bin/greenbone-feed-sync" gvm
